35d4b37118d3e9fc0f11846a7a93c438a5a6388c
Ana Buba 3D <136463588+aninhabuba@users.noreply.github.com>
2025-07-08 17:32:33 -0300

    ...

--- U4_2T/js/script.js
+++ U4_2T/js/script.js
@@ -59,7 +59,7 @@ function jogaNovamente(){
 
 function substituiNome(){
     for(const pergunta of perguntas){
-        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g,nome);
     }
 }
 
