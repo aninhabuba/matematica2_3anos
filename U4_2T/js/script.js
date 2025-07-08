1db15574e5134d9a63c2b70b720ff35a1140ac22
Ana Buba 3D <136463588+aninhabuba@users.noreply.github.com>
2025-07-08 16:24:53 -0300

    .

--- U4_2T/js/script.js
+++ U4_2T/js/script.js
@@ -6,7 +6,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado")
-
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 
 
@@ -45,9 +45,14 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049, Ana...";
     textoResultado.textContent= historiaFinal;
     caixaAlternativas.textContent = "";
+    botaoJogarNovamente.addEventListener("click", jogaNovamente());
 }
 
-
+function jogaNovamente(){
+    atual = 0;
+    historiaFinal= "";
+    mostraPergunta();
+}
 
 
 mostraPergunta();
